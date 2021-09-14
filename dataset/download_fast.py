bc33a5e309c6f0d1eb58a7fdc140d8e73bcb1d4e
pepes97 <svevapepe@yahoo.com>
2021-09-14 21:21:41 +0200

    glove

--- dataset/download_fast.py
+++ dataset/download_fast.py
@@ -2,8 +2,8 @@ import os
 import gdown
 
 def download_dataset():
-    URL = "https://drive.google.com/uc?export=download&id=1-FOOKAdTYFkSVKTpfAwQ7gL-BWi5SxbU"
-    gdown.download(URL, os.getcwd()+os.sep+"wiki-news-300d-1M.vec", quiet=False)
+    URL = "https://drive.google.com/uc?export=download&id=1-6gxK7kQODberN-XMP7VWH9QFFcC2nL5"
+    gdown.download(URL, os.getcwd()+os.sep+"glove.6B.300d.txt", quiet=False)
     print("\n")
 
 if __name__ == '__main__':
